8d1e4807c2a9135b0da8c1c3ceb24347e88018b1
n.tldr <42711253+NiteshTuladhar@users.noreply.github.com>
2021-10-28 12:34:19 +0545

    Async storage added for the darkMode

--- components/switch_button.components.js
+++ components/switch_button.components.js
@@ -3,13 +3,22 @@ import { Switch } from 'react-native-paper';
 import { appTheme } from '../constants';
 import { NotesContext } from '../context/notes.context';
 
+
+
 const SwitchButton = () => {
     
     const {darkModeState, isSwitchOn,setIsSwitchOn} = useContext(NotesContext);
     
-    const onToggleSwitch = () => {
-        setIsSwitchOn(!isSwitchOn)
-        darkModeState()
+    const onToggleSwitch = async () => {
+        try{
+            
+            setIsSwitchOn(!isSwitchOn)
+            darkModeState()
+            
+        }catch(e){
+            console.log(`error`, e)
+        }
+        
     };
     return (
         <Switch value={isSwitchOn} onValueChange={onToggleSwitch} color={appTheme.COLORS.primary} />
--- context/notes.context.js
+++ context/notes.context.js
@@ -1,7 +1,7 @@
 import React, { useState, createContext , useEffect} from 'react'
 import { StyleSheet } from 'react-native'
 import axios from 'axios';
-
+import AsyncStorage from '@react-native-async-storage/async-storage';
 export const NotesContext = createContext();
 
 const NotesContextProvider = ({ children }) => {
@@ -18,14 +18,82 @@ const NotesContextProvider = ({ children }) => {
     const counter = Object.keys(notes?notes : 0).length ? Object.keys(notes).length : '0'
     //const counter = '15'
 
-    //Switch
-    const [isSwitchOn, setIsSwitchOn] = React.useState(false);
+    //DarkMode Switch
+    const [isSwitchOn, setIsSwitchOn] = useState(false);
 
     //Search
     const [searchQuery, setSearchOuery] = useState('');
 
     //GetStartedScreen
     const [getStarted, setGetStarted] = useState(false)
+
+     useEffect(() => {
+
+        setIsLoading(true)
+        getData();
+
+    }, [])
+
+    useEffect(() => {
+        
+        getStoreData()
+        getSwitchToggleData()
+        
+    }, [])
+
+
+    useEffect(() => {
+    
+        storeData()
+
+    }, [darkMode, isSwitchOn])
+
+    const storeData = async () => {
+        try {
+            await AsyncStorage.setItem('@darkModeState', (darkMode).toString())
+            await AsyncStorage.setItem('@darkModeSwitchToggle', (isSwitchOn).toString())
+        } catch (e) {
+           console.log(`error`, e)
+        }
+    }
+
+    const getStoreData = async () => {
+        try {
+            const value = await AsyncStorage.getItem('@darkModeState')
+            if(value !== null) {
+                //converting sting to boolean as async storage does not store boolean
+               
+                if (value === 'true'){
+                    var val = JSON.parse("true".toLowerCase());
+                }else{
+                    var val = JSON.parse("false".toLowerCase());
+
+                }
+                setDarkMode(val)
+            }
+        } catch(e) {
+            // error reading value
+        }
+    }
+
+    const getSwitchToggleData = async () => {
+        try {
+            const value = await AsyncStorage.getItem('@darkModeSwitchToggle')
+            if(value !== null) {
+                //converting sting to boolean as async storage does not store boolean
+               
+                if (value === 'true'){
+                    var val = JSON.parse("true".toLowerCase());
+                }else{
+                    var val = JSON.parse("false".toLowerCase());
+
+                }
+                setIsSwitchOn(val)
+            }
+        } catch(e) {
+            // error reading value
+        }
+    }
 
     const getData = async () => {
         setIsLoading(true)
@@ -133,6 +201,7 @@ const NotesContextProvider = ({ children }) => {
 
      const darkModeState = () => {
         setDarkMode(!darkMode);
+       
      }
 
      const modalVisibleState = (id) => {
@@ -140,19 +209,11 @@ const NotesContextProvider = ({ children }) => {
         setCurrentModalValue(id)
      }
 
-    useEffect(() => {
-
-        setIsLoading(true)
-         setTimeout(function() { 
-             getData();
-        
-     }, 7000);
-
-    }, [])
+   
 
     return (
         <NotesContext.Provider value={{ notes,counter, getData,isLoading, error , addNotes, getNoteDetails ,
-                        noteDetails,updateNotes,deleteNotes ,darkMode, searchQuery,setSearchOuery,darkModeState, modalVisibleState, visible, 
+                        noteDetails,updateNotes,deleteNotes ,darkMode, searchQuery,setSearchOuery,storeData,darkModeState, modalVisibleState, visible, 
                         currentModalValue,isSwitchOn,setIsSwitchOn,setGetStarted,getStarted}}>
             {children}
         </NotesContext.Provider>
